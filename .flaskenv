ee0b149eba2a09b77a05811c90ed7d44024fb690
jonchoi88 <jon.c.88@gmail.com>
2020-07-15 08:08:06 +1000

    remove files in Gi Work folder

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=storeapi.py
-FLASK_ENV=development 
--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask,jsonify
-
-app = Flask(__name__)
-#app.config['JSON_SORT_KEYS'] = False
-
-from app import routes,storage
